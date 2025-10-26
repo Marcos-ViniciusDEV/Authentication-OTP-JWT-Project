1415e7fa4a8fc0d937009bba53e94083180d0b53
Marcos Vinicius <marcosmarcosviniciusdigital@gmail.com>
2025-10-26 06:34:12 -0300

    Initial commit

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express, { urlencoded } from "express";
+import helmet from "helmet";
+import cors from "cors";
+import { mainRouter } from "./routes/main";
+
+const app = express();
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+app.use(urlencoded({ extended: true }));
+
+app.use(mainRouter);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server rodando http://localhost:${port}/`);
+});
